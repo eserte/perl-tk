9155a54c16934b7456c2d3889167b625001e463d
Slaven Rezic <slaven@rezic.de>
2006-12-12 22:21:25 +0000

    updated MANIFEST
    git-svn-id: https://svn.perl.org/modules/Tk/trunk@8995 50811bd7-b8ce-0310-adc1-d9db26280581
    
    r1359@biokovo:  eserte | 2006-12-12 23:10:04 +0100

--- JPEG/JPEG.pm
+++ JPEG/JPEG.pm
@@ -33,17 +33,24 @@ Tk::JPEG - JPEG loader for Tk::Photo
 
 =head1 DESCRIPTION
 
-This is an extension for Tk800.015 and later which supplies
+This is an extension for Tk which supplies
 JPEG format loader for Photo image type.
-
-This version also works with Tk804 series.
 
 JPEG access is via release 5 of the The Independent JPEG Group's (IJG)
 free JPEG software.
 
+=head1 HISTORY
+
+This extension works for Tk800.015 and later and is by default bundled
+with Perl/Tk since Tk804.
+
 =head1 AUTHOR
 
 Nick Ing-Simmons E<lt>nick@ing-simmons.netE<gt>
+
+=head1 SEE ALSO
+
+L<Tk::Photo>.
 
 =cut
 
--- PNG/PNG.pm
+++ PNG/PNG.pm
@@ -30,13 +30,20 @@ Tk::PNG - PNG loader for Tk::Photo
 
 =head1 DESCRIPTION
 
-This is an extension for Tk800.* which supplies
+This is an extension for Tk which supplies
 PNG format loader for Photo image type.
 
+=head1 HISTORY
+
+This extension is by default bundled with Perl/Tk since Tk804.
 
 =head1 AUTHOR
 
 Nick Ing-Simmons E<lt>nick@ing-simmons.netE<gt>
+
+=head1 SEE ALSO
+
+L<Tk::Photo>.
 
 =cut
 
