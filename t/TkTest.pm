cc9538cd62d8995fea3f55f26a600c3aa361a6ac
Slaven Rezic <slaven@rezic.de>
2007-01-04 23:13:30 +0000

    last change also introduced catch_grabs; copyright year
    git-svn-id: https://svn.perl.org/modules/Tk/trunk@9032 50811bd7-b8ce-0310-adc1-d9db26280581
    
    r1431@biokovo:  eserte | 2007-01-05 00:10:37 +0100

--- t/TkTest.pm
+++ t/TkTest.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2003 Slaven Rezic. All rights reserved.
+# Copyright (C) 2003,2006,2007 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 
