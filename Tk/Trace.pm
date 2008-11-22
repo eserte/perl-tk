c0854f1e0d23d784e35f9add80abc993422d6954
Steve Lidie <sol0@Lehigh.EDU>
2008-11-22 00:12:03 +0000

    * avoid autovivifying %TRACE values, fixes   http://rt.cpan.org/Ticket/Display.html?id=41069 and makes Tk::Gauge   work (patch by Steve Lidie)
    git-svn-id: https://svn.perl.org/modules/Tk/trunk@12096 50811bd7-b8ce-0310-adc1-d9db26280581

--- Tk/Trace.pm
+++ Tk/Trace.pm
@@ -178,10 +178,12 @@ sub Tk::Widget::traceVdelete {
 
     my ( $parent, $vref, $op_not_honored, $callabck_not_honored ) = @_;
 
-    if ( defined $TRACE{$vref}->[0] ) {
-        $$vref = $TRACE{$vref}->[0]->Fetch;
-        $TRACE{$vref}->[0]->Unwatch;
-        delete $TRACE{$vref};
+    if ( defined $TRACE{$vref} ) {
+	if ( defined $TRACE{$vref}->[0] ) {
+	    $$vref = $TRACE{$vref}->[0]->Fetch;
+	    $TRACE{$vref}->[0]->Unwatch;
+	    delete $TRACE{$vref};
+	}
     }
 
 } # end traceVdelete
