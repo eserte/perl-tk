b5b2f9bf43207759e2b2807d80029347ece72732
Slaven Rezic <slaven@rezic.de>
2010-05-27 19:16:53 +0000

    * inc'd versions
    git-svn-id: https://svn.perl.org/modules/Tk/trunk@14067 50811bd7-b8ce-0310-adc1-d9db26280581

--- Event/Event.pm
+++ Event/Event.pm
@@ -1,8 +1,8 @@
 package Tk::Event;
 use vars qw($VERSION $XS_VERSION @EXPORT_OK);
 END { CleanupGlue() }
-$VERSION = '4.023'; # was: sprintf '4.%03d', q$Revision: #15 $ =~ /\D(\d+)\s*$/;
-$XS_VERSION = '804.028_503';
+$VERSION = '4.024'; # was: sprintf '4.%03d', q$Revision: #15 $ =~ /\D(\d+)\s*$/;
+$XS_VERSION = '804.029';
 $XS_VERSION = eval $XS_VERSION;
 use base  qw(Exporter);
 use XSLoader;
