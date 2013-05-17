382cf6dd2c8fa8da767b0cad53045afbc16bb614
Slaven Rezic <slaven@rezic.de>
2013-05-17 23:54:26 +0200

    * inc'd VERSION to 804.030_500

--- Event/Event.pm
+++ Event/Event.pm
@@ -2,7 +2,7 @@ package Tk::Event;
 use vars qw($VERSION $XS_VERSION @EXPORT_OK);
 END { CleanupGlue() }
 $VERSION = '4.024'; # was: sprintf '4.%03d', q$Revision: #15 $ =~ /\D(\d+)\s*$/;
-$XS_VERSION = '804.030';
+$XS_VERSION = '804.030_500';
 $XS_VERSION =~ s{_}{};
 use base  qw(Exporter);
 use XSLoader;
