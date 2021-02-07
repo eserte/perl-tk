63278cffcf5c2dfa45c991f8425cd0995643117d
Slaven Rezic <slaven@rezic.de>
2021-02-07 18:22:42 +0100

    update versions

--- Event/Event.pm
+++ Event/Event.pm
@@ -1,8 +1,8 @@
 package Tk::Event;
 use vars qw($VERSION $XS_VERSION @EXPORT_OK);
 END { CleanupGlue() }
-$VERSION = '4.038';
-$XS_VERSION = '804.035';
+$VERSION = '4.039';
+$XS_VERSION = '804.035_501';
 $XS_VERSION =~ s{_}{};
 use base  qw(Exporter);
 use XSLoader;
